420008193b46ad9ab76d125e649d0334b23d2a27
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-01-16 15:56:31 -0300

    ItemCard create

--- lib/screen/home/components/body.dart
+++ lib/screen/home/components/body.dart
@@ -11,7 +11,32 @@ class Body extends StatelessWidget {
     return Column(
       children: [
         SearchBox(onChanged: (String value) {}),
-        CategoryList(),
+        const CategoryList(),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              ItemCard(
+                title: 'Hambúrguer & Cerveja',
+                shopName: "McDonald's",
+                svgSrc: 'assets/icons/burger_beer.svg',
+                press: () {},
+              ),
+              ItemCard(
+                title: 'Macarrão chinês',
+                shopName: "Wendys",
+                svgSrc: 'assets/icons/chinese_noodles.svg',
+                press: () {},
+              ),
+              ItemCard(
+                title: 'Hambúrguer & Cerveja',
+                shopName: "McDonald's",
+                svgSrc: 'assets/icons/burger_beer.svg',
+                press: () {},
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
--- lib/screen/home/components/components.dart
+++ lib/screen/home/components/components.dart
@@ -2,3 +2,4 @@ export 'app_bar.dart';
 export 'body.dart';
 export 'category_item.dart';
 export 'category_list.dart';
+export 'item_card.dart';
