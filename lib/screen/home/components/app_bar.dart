29f25eecbba1e7b07133b1b0955b1d83a3b1c528
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-01-16 12:16:42 -0300

    HomeAppBar create

---
+++ lib/screen/home/components/app_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../constants.dart';
+
+AppBar homeAppBar(BuildContext context) {
+  return AppBar(
+    centerTitle: true,
+    backgroundColor: Colors.white,
+    elevation: 0,
+    leading: IconButton(
+      icon: SvgPicture.asset('assets/icons/menu.svg'),
+      onPressed: () {},
+    ),
+    title: RichText(
+      text: TextSpan(
+        style: Theme.of(context)
+            .textTheme
+            .titleLarge
+            ?.copyWith(fontWeight: FontWeight.bold),
+        children: const [
+          TextSpan(
+            text: 'Punk',
+            style: TextStyle(color: kSecondaryColor),
+          ),
+          TextSpan(
+            text: 'Food',
+            style: TextStyle(color: kPrimaryColor),
+          ),
+        ],
+      ),
+    ),
+    actions: [
+      IconButton(
+        icon: SvgPicture.asset('assets/icons/notification.svg'),
+        onPressed: () {},
+      )
+    ],
+  );
+}
--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'components/components.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: homeAppBar(context),
+    );
   }
 }
