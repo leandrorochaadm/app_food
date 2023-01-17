b6d679af9f49022a80c7e058f04e2027fb1e3ee5
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-01-17 14:52:19 -0300

    DetailsScreen body move own file

--- lib/screen/details/components/components.dart
+++ lib/screen/details/components/components.dart
@@ -1,4 +1,5 @@
 export 'app_bar.dart';
+export 'body.dart';
 export 'item_image.dart';
 export 'order_button.dart';
 export 'title_price_rating.dart';
--- lib/screen/details/details_screen.dart
+++ lib/screen/details/details_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../constants.dart';
 import 'components/components.dart';
@@ -16,75 +15,3 @@ class DetailsScreen extends StatelessWidget {
     );
   }
 }
-
-class Body extends StatelessWidget {
-  const Body({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const ItemImage(imgSrc: 'assets/images/burger.png'),
-        const Expanded(
-          child: ItemInfo(),
-        )
-      ],
-    );
-  }
-}
-
-class ItemInfo extends StatelessWidget {
-  const ItemInfo({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-
-    return Container(
-      padding: const EdgeInsets.all(20),
-      width: double.infinity,
-      decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30),
-            topRight: Radius.circular(30),
-          )),
-      child: Column(
-        children: [
-          shopeName(name: "MacDonald's"),
-          TitlePriceRating(
-            name: 'Hamburger',
-            rating: 4,
-            numOfReviews: 24,
-            price: 15,
-            onRatingChanged: (double rating) {},
-          ),
-          Text(
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus semper pulvinar tempus. Cras id urna. '
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus semper pulvinar tempus. Cras id urna. '
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus semper pulvinar tempus. Cras id urna.',
-            style: TextStyle(height: 1.5),
-          ),
-          SizedBox(height: size.height * 0.1),
-          OrderButton(press: () {})
-        ],
-      ),
-    );
-  }
-
-  Row shopeName({required String name}) {
-    return Row(
-      children: [
-        const Icon(
-          Icons.location_on,
-          color: kSecondaryColor,
-        ),
-        const SizedBox(width: 10),
-        Text(name),
-      ],
-    );
-  }
-}
-
