2739f23d17eb1672e6209d1a1e18aa5de8c2e1f2
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-01-17 09:40:59 -0300

    ItemInfo create

--- lib/screen/details/details_screen.dart
+++ lib/screen/details/details_screen.dart
@@ -24,7 +24,30 @@ class Body extends StatelessWidget {
     return Column(
       children: [
         const ItemImage(imgSrc: 'assets/images/burger.png'),
+        const Expanded(
+          child: ItemInfo(),
+        )
       ],
+    );
+  }
+}
+
+class ItemInfo extends StatelessWidget {
+  const ItemInfo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      width: double.infinity,
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          )),
     );
   }
 }
