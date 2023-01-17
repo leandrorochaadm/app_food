5beb1692567cba03b68ef3219b89b189f25b3377
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-01-17 10:25:40 -0300

    PricerCliper ItemInfo create

--- lib/screen/details/details_screen.dart
+++ lib/screen/details/details_screen.dart
@@ -48,9 +48,37 @@ class ItemInfo extends StatelessWidget {
             topLeft: Radius.circular(30),
             topRight: Radius.circular(30),
           )),
-      child: Column(children: [
-        shopeName(name: "MacDonald's"),
-      ]),
+      child: Column(
+        children: [
+          shopeName(name: "MacDonald's"),
+          Row(
+            children: [
+              Column(
+                children: [
+                  //
+                ],
+              ),
+              ClipPath(
+                clipper: PricerCliper(),
+                child: Container(
+                  alignment: Alignment.topCenter,
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  height: 66,
+                  width: 65,
+                  color: kPrimaryColor,
+                  child: Text(
+                    '\$15',
+                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -65,5 +93,24 @@ class ItemInfo extends StatelessWidget {
         Text(name),
       ],
     );
+  }
+}
+
+class PricerCliper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    double ignoreHeight = 20;
+    path.lineTo(0, size.height - ignoreHeight);
+    path.lineTo(size.width / 2, size.height);
+    path.lineTo(size.width, size.height - ignoreHeight);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
